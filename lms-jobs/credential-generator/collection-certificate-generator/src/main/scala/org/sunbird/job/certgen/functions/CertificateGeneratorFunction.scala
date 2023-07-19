5d4d343143196031ac004c5e765f6683bc4503be
Shishir Suman <74050813+shishirsuman092@users.noreply.github.com>
2023-07-19 20:17:31 +0530

    Update CertificateGeneratorFunction.scala

--- lms-jobs/credential-generator/collection-certificate-generator/src/main/scala/org/sunbird/job/certgen/functions/CertificateGeneratorFunction.scala
+++ lms-jobs/credential-generator/collection-certificate-generator/src/main/scala/org/sunbird/job/certgen/functions/CertificateGeneratorFunction.scala
@@ -250,7 +250,7 @@ class CertificateGeneratorFunction(config: CertificateGeneratorConfig, httpUtil:
         val httpResponse = httpUtil.post(searchUri, req)
         logger.info("RC Search API httpResponse: " + httpResponse)
         if(httpResponse.status == 200) {
-          val resp = ScalaJsonUtil.deserialize[List[Map[String, AnyRef]]](httpResponse.body)
+          //val resp = ScalaJsonUtil.deserialize[List[Map[String, AnyRef]]](httpResponse.body)
           //id = resp.head.getOrElse("osid", null).asInstanceOf[String]
           id=""
         }
